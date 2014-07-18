272c5754f53decc64562d2922e3ca48728b4df8e
Scott Merrill <smerrill@covermymeds.com>
2014-07-18 09:05:08 -0400

    document gluster::volume::option

--- manifests/volume/option.pp
+++ manifests/volume/option.pp
@@ -1,3 +1,39 @@
+# == Define: gluster::volume::option
+#
+# set or remove a Gluster volume option
+#
+# === Parameters
+#
+# $title: the name of the option to (re)set
+# $volume: the name of the Gluster volume on which to operate
+# $value: the value to set for this option
+# $remove: whether to remove a previously-set option
+#
+# === Examples
+#
+# gluster::volume::option { 'nfs.disable':
+#   volume => 'gv0',
+#   value  => 'on',
+# }
+#
+# gluster::volume::option { 'server.allow-insecure':
+#   volume => 'gv0',
+#   value  => 'on',
+# }
+#
+# gluster::volume::option { 'feature.read-only':
+#   volume => 'gv0',
+#   remove => true,
+# }
+#
+# === Authors
+#
+# Scott Merrill <smerrill@covermymeds.com>
+#
+# === Copyright
+#
+# Copyright 2014 CoverMyMeds, unless otherwise noted
+#
 define gluster::volume::option (
   $volume = undef,
   $value   = undef,
