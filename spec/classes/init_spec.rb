700f5b09034b0ae1cf6b95c1cdc0a5cae8a34bcd
Tim Meusel <tim@bastelfreak.de>
2016-05-09 16:12:04 +0200

    mock custom facts

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -55,6 +55,13 @@ describe 'gluster', :type => :class do
       end
 
       context 'when volumes defined' do
+        let :facts do
+          super().merge(
+            gluster_binary: '/sbin/gluster',
+            gluster_peer_list: 'example1,example2',
+            gluster_volume_list: 'gl1.example.com:/glusterfs/backup,gl2.example.com:/glusterfs/backup'
+          )
+        end
         let :params do
           { :volumes =>
             {
