da772ff42d34450f98d3c03b783c4b0815767f03
Tim Meusel <tim@bastelfreak.de>
2016-05-09 15:41:13 +0200

    use correct scoping in repo_yum_spec.rb

--- spec/classes/repo_yum_spec.rb
+++ spec/classes/repo_yum_spec.rb
@@ -2,53 +2,55 @@ require 'spec_helper'
 
 describe 'gluster::repo::yum', type: :class do
   on_supported_os.each do |os, facts|
-    context "on #{os} with all defaults" do
+    context "on #{os}" do
       let(:facts) do
         facts
       end
-      it 'should install' do
-        should_not create_package('yum-plugin-priorities')
-        should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-        should create_yumrepo('glusterfs-x86_64').with(
-          enabled: 1,
-          baseurl: 'https://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-6/x86_64/',
-          gpgcheck: 1,
-          gpgkey: 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub',
-        )
-      end
-    end
-    describe 'bogus version' do
-      let :params do { version: 'foobar', } end
-      it 'should not install' do
-        expect {
+      context 'with all defaults' do
+        it 'should install' do
+          should_not create_package('yum-plugin-priorities')
           should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-        }.to raise_error(Puppet::Error, /doesn't make sense!/)
+          should create_yumrepo('glusterfs-x86_64').with(
+            enabled: 1,
+            baseurl: 'https://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-6/x86_64/',
+            gpgcheck: 1,
+            gpgkey: 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub',
+          )
+        end
       end
-    end
-    describe 'unsupported architecture' do
-      let :facts do { architecture: 'zLinux', } end
-      it 'should not install' do
-        expect {
+      context 'bogus version' do
+        let :params do { version: 'foobar', } end
+        it 'should not install' do
+          expect {
+            should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
+          }.to raise_error(Puppet::Error, /doesn't make sense!/)
+        end
+      end
+      context 'unsupported architecture' do
+        let :facts do { architecture: 'zLinux', } end
+        it 'should not install' do
+          expect {
+            should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
+          }.to raise_error(Puppet::Error, /not yet supported/)
+        end
+      end
+      context 'latest Gluster with priority' do
+        let :params do
+          {
+            priority: '50',
+          }
+        end
+        it 'should install' do
           should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-        }.to raise_error(Puppet::Error, /not yet supported/)
-      end
-    end
-    context 'latest Gluster with priority' do
-      let :params do
-        {
-          priority: '50',
-        }
-      end
-      it 'should install' do
-        should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-        should create_package('yum-plugin-priorities')
-        should create_yumrepo('glusterfs-x86_64').with(
-          enabled: 1,
-          baseurl: 'https://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-6/x86_64/',
-          gpgcheck: 1,
-          gpgkey: 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub',
-          priority: '50',
-        )
+          should create_package('yum-plugin-priorities')
+          should create_yumrepo('glusterfs-x86_64').with(
+            enabled: 1,
+            baseurl: 'https://download.gluster.org/pub/gluster/glusterfs/LATEST/EPEL.repo/epel-6/x86_64/',
+            gpgcheck: 1,
+            gpgkey: 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub',
+            priority: '50',
+          )
+        end
       end
     end
   end
