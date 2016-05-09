11d4e7bdff778b669f56414ebc288c72013297be
Tim Meusel <tim@bastelfreak.de>
2016-05-09 15:10:37 +0200

    implement rspec-puppet-facts in service_spec.rb

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe 'gluster::service', type: :class do
-  it 'should start the service' do
-    should create_service('glusterd')
+  on_supported_os.each do |os, facts|
+    context "on #{os} with all defaults" do
+      let(:facts) do
+        facts
+      end
+      it { should compile.with_all_deps }
+      it 'should start the service' do
+        should create_service('glusterd')
+      end
+    end
   end
 end
