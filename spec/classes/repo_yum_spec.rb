f6c9df1097c290e0429d761331107ff9a0c91622
Tim Meusel <tim@bastelfreak.de>
2016-05-09 14:34:07 +0200

    implement rspec-puppet-facts in repo_yum_spec

--- spec/classes/repo_yum_spec.rb
+++ spec/classes/repo_yum_spec.rb
@@ -1,45 +1,10 @@
 require 'spec_helper'
 
 describe 'gluster::repo::yum', type: :class do
-  describe 'version not specified' do
-    it 'should not install' do
-      expect {
-        should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-      }.to raise_error(Puppet::Error, /Version not specified/)
-    end
-  end
-  describe 'bogus version' do
-    let :params do { version: 'foobar', } end
-    it 'should not install' do
-      expect {
-        should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-      }.to raise_error(Puppet::Error, /doesn't make sense!/)
-    end
-  end
-  describe 'unsupported architecture' do
-    let :facts do { architecture: 'zLinux', } end
-    let :params do { version: 'LATEST', } end
-    it 'should not install' do
-      expect {
-        should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
-      }.to raise_error(Puppet::Error, /not yet supported/)
-    end
-  end
-  describe 'Red Hat Enterprise Linux' do
-    context 'latest Gluster on RHEL 6 x86_64' do
-      let :facts do
-        {
-          architecture: 'x86_64',
-          operatingsystemmajrelease: '6',
-        }
-      end
-      let :params do
-        {
-          version: 'LATEST',
-          repo_key_path: '/etc/pki/rpm-gpg/',
-          repo_key_name: 'RPM-GPG-KEY-gluster.pub',
-          repo_key_source: 'puppet:///modules/gluster/RPM-GPG-KEY-gluster.pub',
-        }
+  on_supported_os.each do |os, facts|
+    context "on #{os} with all defaults" do
+      let(:facts) do
+        facts
       end
       it 'should install' do
         should_not create_package('yum-plugin-priorities')
@@ -52,19 +17,25 @@ describe 'gluster::repo::yum', type: :class do
         )
       end
     end
-    context 'latest Gluster on RHEL 6 x86_64 with priority' do
-      let :facts do
-        {
-          architecture: 'x86_64',
-          operatingsystemmajrelease: '6',
-        }
+    describe 'bogus version' do
+      let :params do { version: 'foobar', } end
+      it 'should not install' do
+        expect {
+          should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
+        }.to raise_error(Puppet::Error, /doesn't make sense!/)
       end
+    end
+    describe 'unsupported architecture' do
+      let :facts do { architecture: 'zLinux', } end
+      it 'should not install' do
+        expect {
+          should create_file('/etc/pki/rpm-gpg/RPM-GPG-KEY-gluster.pub')
+        }.to raise_error(Puppet::Error, /not yet supported/)
+      end
+    end
+    context 'latest Gluster with priority' do
       let :params do
         {
-          version: 'LATEST',
-          repo_key_path: '/etc/pki/rpm-gpg/',
-          repo_key_name: 'RPM-GPG-KEY-gluster.pub',
-          repo_key_source: 'puppet:///modules/gluster/RPM-GPG-KEY-gluster.pub',
           priority: '50',
         }
       end
