302f0399bc522c21ef85dcf410a9d4ea8e2ae09b
Tim Meusel <tim@bastelfreak.de>
2016-05-09 14:33:56 +0200

    implement rspec-puppet-facts in install_spec.rb

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -9,23 +9,22 @@ describe 'gluster::install', type: :class do
       }.to raise_error(Puppet::Error, /not yet supported/)
     end
   end
-  describe 'installing on Red Hat Enterprise Linux' do
-    let :facts do
-      {
-        osfamily: 'RedHat',
-        operatingsystem: 'RedHat',
-        operatingsystemmajrelease: '6',
-        architecture: 'x86_64',
-      }
-    end
-    context 'when repo is true' do
-      let :params do
-        { repo: true }
+  on_supported_os.each do |os, facts|
+    context "on #{os} with all defaults" do
+      let(:facts) do
+        facts
       end
+      it { should compile.with_all_deps }
       it 'should create gluster::repo' do
         should create_class('gluster::repo').with(
           version: 'LATEST',
         )
+      end
+      it 'should install glusterfs-fuse package' do
+        should create_package('glusterfs-fuse')
+      end
+      it 'should install glusterfs-server' do
+        should create_package('glusterfs-server')
       end
     end
     context 'when repo is false' do
@@ -36,28 +35,12 @@ describe 'gluster::install', type: :class do
         should_not create_class('gluster::repo')
       end
     end
-    context 'when client is true' do
-      let :params do
-        { client: true }
-      end
-      it 'should install glusterfs-fuse package' do
-        should create_package('glusterfs-fuse')
-      end
-    end
     context 'when client is false' do
       let :params do
         { client: false }
       end
       it 'should not install glusterfs-fuse package' do
         should_not create_package('glusterfs-fuse')
-      end
-    end
-    context 'when server is true' do
-      let :params do
-        { server: true }
-      end
-      it 'should install glusterfs-server' do
-        should create_package('glusterfs-server')
       end
     end
     context 'when server is false' do
