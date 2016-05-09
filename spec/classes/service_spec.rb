9487707a66b9aa2477a4a851a122359c347df7e1
Tim Meusel <tim@bastelfreak.de>
2016-05-09 15:42:00 +0200

    use correct scoping in service_spec.rb

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -2,13 +2,15 @@ require 'spec_helper'
 
 describe 'gluster::service', type: :class do
   on_supported_os.each do |os, facts|
-    context "on #{os} with all defaults" do
+    context "on #{os}" do
       let(:facts) do
         facts
       end
-      it { should compile.with_all_deps }
-      it 'should start the service' do
-        should create_service('glusterd')
+      context 'with all defaults' do
+        it { should compile.with_all_deps }
+        it 'should start the service' do
+          should create_service('glusterd')
+        end
       end
     end
   end
