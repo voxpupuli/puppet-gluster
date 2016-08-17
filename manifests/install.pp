2cd9fe861dd4255b452efcb5201b4dc7b623d668
Tim Meusel <tim@bastelfreak.de>
2016-08-17 19:25:52 +0200

    extend install logic
    we've some cases here. We Install glusterfs maybe on:
    * a client
    * a server
    
    and that on multiple distributions. where the package names can be the
    same. The server package needs a different attribute.

--- manifests/install.pp
+++ manifests/install.pp
@@ -39,7 +39,7 @@ class gluster::install (
 
   validate_bool($server)
   validate_bool($client)
-  validate_bool($version)
+  validate_bool($repo)
 
   if $repo {
     # install the correct repo
@@ -57,19 +57,36 @@ class gluster::install (
     default  => $version,
   }
 
-  if $client {
-    # we use ensure_packages here because on some distributions the client and server package have different names
-    ensure_packages($client_package, {
-      ensure => $_version,
-      tag    => 'gluster-packages',
-    })
-  }
-  if $server {
-    # we use ensure_packages here because on some distributions the client and server package have different names
-    ensure_packages($server_package, {
-      ensure => $_version,
-      notify => Class[::gluster::service],
-      tag    => 'gluster-packages',
-    })
+  if $client_package == $server_package {
+    if $server {
+      # we use ensure_packages here because on some distributions the client and server package have different names
+      ensure_packages($server_package, {
+        ensure => $_version,
+        tag    => 'gluster-packages',
+        notify => Class[::gluster::service],
+      })
+    } elsif $client {
+      ensure_packages($client_package, {
+        ensure => $_version,
+        tag    => 'gluster-packages',
+      })
+    }
+  } else {
+    if $client {
+      # we use ensure_packages here because on some distributions the client and server package have different names
+      ensure_packages($client_package, {
+        ensure => $_version,
+        tag    => 'gluster-packages',
+      })
+    }
+
+    if $server {
+      # we use ensure_packages here because on some distributions the client and server package have different names
+      ensure_packages($server_package, {
+        ensure => $_version,
+        notify => Class[::gluster::service],
+        tag    => 'gluster-packages',
+      })
+    }
   }
 }
