8abaaae3fceb3811b0973e42e5ed37bba925577d
Nelo Wallus <nelo@wallus.de>
2016-12-12 21:48:52 +0100

    Add spec test for passed replica count

--- spec/defines/volume_spec.rb
+++ spec/defines/volume_spec.rb
@@ -42,6 +42,23 @@ describe 'gluster::volume', type: :define do
       it { is_expected.to compile }
     end
 
+    describe 'with replica count' do
+      let(:title) { 'storage1' }
+      let(:facts) {
+          {
+              gluster_binary: '/usr/bin/gluster',
+              gluster_peer_list: 'srv1.local,srv2.local',
+              gluster_volume_list: 'storage2',
+          }
+      }
+
+      it do
+          is_expected.to compile
+          is_expected.to contain_exec('gluster create volume storage1') \
+              .with_command(/replica [\d]+/)
+      end
+    end
+
     describe 'no replica count' do
       let(:title) { 'storage1' }
       let(:params) {
