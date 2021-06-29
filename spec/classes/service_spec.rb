c5501ac990f80408785f4664395bfd67e304a190
Kotty <kotty@guns-n-girls.de>
2021-06-29 15:29:24 +0200

    debug output

--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -23,8 +23,7 @@ describe 'gluster::service', type: :class do
                       when 'Archlinux'
                         'glusterd'
                      end
-      print 'ServiceName:'
-      pp service_name
+      pp "ServiceName: ", service_name, "Family: ", facts[:os]['family'], "Version: ", facts[:os]['release']['major']
 
       context 'with all defaults' do
         it { is_expected.to compile.with_all_deps }
