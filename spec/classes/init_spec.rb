4e707fb8f6350cd1312877b7331fd11fe82d1fcd
Kotty <kotty@guns-n-girls.de>
2021-06-30 12:52:14 +0200

    next test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -123,8 +123,8 @@ describe 'gluster', type: :class do
       context 'when volumes defined' do
         let :facts do
           super().merge(
-            gluster_binary: '/sbin/gluster',
-            gluster_peer_list: 'example1,example2',
+            gluster_binary: '/sbin/gluster'
+            gluster_peer_list: 'example1,example2'
             gluster_volume_list: 'gl1.example.com:/glusterfs/backup,gl2.example.com:/glusterfs/backup'
           )
         end
@@ -154,8 +154,8 @@ describe 'gluster', type: :class do
       context 'when volumes defined without replica' do
         let(:facts) do
           super().merge(
-            gluster_binary: '/sbin/gluster',
-            gluster_peer_list: 'example1,example2',
+            gluster_binary: '/sbin/gluster'
+            gluster_peer_list: 'example1,example2'
             gluster_volume_list: 'gl1.example.com:/glusterfs/backup,gl2.example.com:/glusterfs/backup'
           )
         end
